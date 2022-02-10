e0f7edf9eff65f024e80616ad15f571f00a3bf84
esubalew.seife@gmail.com <esubalew.seife@gmail.com>
2022-02-10 07:33:11 -0800

    'second-commit'

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 "use strict";
 const imgContainer = document.querySelector(".img-container");
+const btnSearch = document.querySelector(".btn-search");
+const searchInput = document.querySelector(".search-input");
 let result = {
   img: [],
+  qeury: "",
 };
-const getData = async function () {
+const getData = async function (query) {
   const res = await fetch(
-    `https://api.unsplash.com/photos/?client_id=CfwIRurFMXSkezH69Zo059-H8zyIqn3zUiy8x44bgls;`
+    `https://api.unsplash.com/search/photos?query=${query}&client_id=CfwIRurFMXSkezH69Zo059-H8zyIqn3zUiy8x44bgls;`
   );
   const data = await res.json();
-  console.log(data);
-  data.forEach((el) => {
+  data.results.forEach((el) => {
     result.img.push(el.links.download);
   });
   result.img.forEach((el) => {
     const markup = `
-       <figure class="img-fgr">
-         <img src="${el}" alt="" />
-       </figure>
-     `;
+           <figure class="img-fgr">
+             <img src="${el}" alt="" />
+           </figure>
+         `;
+
     imgContainer.insertAdjacentHTML("afterbegin", markup);
   });
-  console.log(imgContainer);
 };
-getData();
+
+btnSearch.addEventListener("click", function (e) {
+  e.preventDefault();
+  result.qeury = searchInput.value;
+  searchInput.value = "";
+  getData(result.qeury);
+});
+
+btnSearch.addEventListener("keydown", function (e) {
+  e.preventDefault();
+  if (e.key === "Enter") result.qeury = searchInput.value;
+  searchInput.value = "";
+  getData(result.qeury);
+});
